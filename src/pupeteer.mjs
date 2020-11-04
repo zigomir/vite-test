3a6d7d5b4eef3df638d7ea0af02b6759f5743755
ziga <zigomir@gmail.com>
2020-11-04 14:11:17 +0100

    POC

---
+++ src/pupeteer.mjs
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer'
+import { createServer } from 'vite'
+const PORT = 3001
+
+;(async () => {
+  const vite = await createServer({}).listen(PORT)
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const address = `http://localhost:${PORT}/test.html`
+  await page.goto(address, { waitUntil: 'domcontentloaded' })
+
+  let exitCode = 0
+  let gotAtLeastOnePass = false
+  page.on('console', (msg) => {
+    if (msg._text === 'fail') {
+      exitCode = 1
+      process.stdout.write('\x1b[31m.') // red dot
+    }
+    if (msg._text === 'pass') {
+      gotAtLeastOnePass = true
+      process.stdout.write('\x1b[32m.') // green dot
+    }
+  })
+
+  await page.evaluate(() => {})
+  await browser.close()
+
+  const passed = exitCode === 0 && gotAtLeastOnePass
+
+  console.log('\x1b[0m') // resets color
+  console.log(
+    passed
+    ? '🤘 Tests passed.'
+    : `💩 Tests failed. Open in real browser to debug while running vite server`
+  )
+
+  vite.close()
+  process.exit(exitCode)
+})()
---
+++ src/test.js
@@ -0,0 +1,27 @@
+ // this is now only available on
+import { mount } from '@vue/test-utils/dist/vue-test-utils.esm-browser'
+// import { mount } from '@vue/test-utils'
+import { assert } from '@esm-bundle/chai'
+import HelloWorld from './components/HelloWorld.vue'
+
+mocha.setup('bdd')
+
+describe('HelloWorld', () => {
+  it('should render msg prop 1', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Sleuth' } })
+    assert(wrapper.html().includes('Sleuth'))
+  })
+
+  it('should increment count', async () => {
+    const wrapper = mount(HelloWorld)
+    assert(wrapper.html().includes('count is: 0'))
+    await wrapper.find('button').trigger('click')
+    assert(wrapper.html().includes('count is: 1'))
+  })
+})
+
+const runner = mocha.run()
+// check https://mochajs.org/api/runner for more events
+runner.on('fail', () => console.log('fail'))
+runner.on('pass', () => console.log('pass'))
+runner.on('end', () => console.log('end'))
