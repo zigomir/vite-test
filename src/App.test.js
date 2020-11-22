0d1d198092c39488fd96dbb7ee87c8e56e5e92bf
zigomir <zigomir@gmail.com>
2020-11-22 17:24:29 +0100

    Export vue test utils from test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils/dist/vue-test-utils.esm-browser' // available since 2.0.0-beta.9 (https://github.com/vuejs/vue-test-utils-next/pull/235)
+import { shallowMount } from '../test/test.js'
 import App from './App.vue'
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
